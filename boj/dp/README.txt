c0766a6a6a035663bd86f454b0e1359e0bc38a8e
ttumzzi <ssumini0829@gmail.com>
2019-08-14 17:28:57 +0900

    boj solutions using cpp

---
+++ boj/dp/README.txt
@@ -0,0 +1,2 @@
+It is boj solutions about Dynamic programming problems.
+I wrote code using cpp.
---
+++ boj/dp/boj_11726.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define DEVIDING_NUM 10007
+
+int main(){
+    int n, a = 0, b = 1, c;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        c = (a + b) % DEVIDING_NUM;
+        a = b;
+        b = c;
+    }
+
+    printf("%d\n", c);
+}
