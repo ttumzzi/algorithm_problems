cbf8dc59d6e7d52414f1acc18741dcf23984da7c
ttumzzi <ssumini0829@gmail.com>
2020-02-13 07:58:22 +0900

    위장

--- programers/42578.cpp
+++ programers/42578.cpp
@@ -1,55 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
-bool visit[31] = {
-    0,
-};
-
-int dfs(vector<int> v, int level)
-{
-    if (level == v.size())
-    {
-        int mult = 1;
-        for (int i = 0; i < v.size(); i++)
-            if (visit[i])
-                mult *= v[i];
-        return mult;
-    }
-    visit[level] = true;
-    int t = dfs(v, level + 1);
-    visit[level] = false;
-    int f = dfs(v, level + 1);
-    return t + f;
-}
 
 int solution(vector<vector<string>> clothes)
 {
-    vector<string> type;
-    vector<int> v;
-    int answer = 0;
+    map<string, int> m;
+    int answer = 1;
     for (int i = 0; i < clothes.size(); i++)
     {
-        int flag = -1;
-        for (int j = 0; j < type.size(); j++)
-        {
-            if (type[j].compare(clothes[i][1]) == 0)
-            {
-                flag = j;
-                break;
-            }
-        }
-        if (flag == -1)
-        {
-            type.push_back(clothes[i][1]);
-            v.push_back(1);
-        }
-        else
-            v[flag]++;
+        m[clothes[i][1]]++;
     }
 
-    answer = dfs(v, 0);
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        answer *= (it->second + 1);
+    }
 
     return answer - 1;
 }
