043d0d763275d2c2eeee050f6bc6637924fa0067
ttumzzi <ssumini0829@gmail.com>
2020-02-13 07:59:13 +0900

    위장

--- programers/42578.cpp
+++ programers/42578.cpp
@@ -10,9 +10,7 @@ int solution(vector<vector<string>> clothes)
     map<string, int> m;
     int answer = 1;
     for (int i = 0; i < clothes.size(); i++)
-    {
         m[clothes[i][1]]++;
-    }
 
     for (auto it = m.begin(); it != m.end(); it++)
         answer *= (it->second + 1);
