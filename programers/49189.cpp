c911b9ce6c8430f9b50405e6c59d686eb3143a15
ttumzzi <ssumini0829@gmail.com>
2020-02-15 08:13:48 +0900

    가장 먼 노드

--- programers/49189.cpp
+++ programers/49189.cpp
@@ -1,42 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
+struct node
+{
+    int idx, level;
+};
 
 int mat[20001][20001];
-int visit[20001][20001];
-int maximum = -1;
-
-void dfs(int n, int number, int level)
-{
-    int cnt = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (mat[i][number] && !visit[i][number])
-        {
-            cnt++;
-            dfs(n, i, level + 1);
-        }
-    }
-    if (!cnt && level > maximum)
-    {
-        maximum = level;
-        cnt = 0;
-    }
-}
+int visit[20001];
 
 int solution(int n, vector<vector<int>> edge)
 {
+    int vec_size = edge.size() + 1;
     int answer = 0;
-    for (auto it = edge.begin(); it != edge.end(); it++)
-        mat[(*it)[0]][(*it)[1]] = 1;
-    dfs(n, 1, 0);
-    answer = maximum;
+    int max = 0;
+    for (int i = 0; i < edge.size(); i++)
+    {
+        mat[edge[i][0]][edge[i][1]] = 1;
+        mat[edge[i][1]][edge[i][0]] = 1;
+    }
+    queue<node> q;
+    visit[1] = 1;
+    q.push({1, 0});
+    while (!q.empty())
+    {
+        int idx = q.front().idx;
+        int level = q.front().level;
+        if (level > max)
+        {
+            max = level;
+            answer = 1;
+        }
+        else if (level == max)
+        {
+            answer++;
+        }
+        q.pop();
+        for (int i = 1; i <= n; i++)
+        {
+            if (mat[i][idx] && !visit[i])
+            {
+                visit[i] = 1;
+                q.push({i, level + 1});
+            }
+        }
+    }
+    cout << answer;
     return answer;
 }
 
 int main()
 {
-    solution(6, vector<vector<int>>{{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}});
+    solution(20000, vector<vector<int>>{{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}});
 }
