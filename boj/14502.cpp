16229e11572832a8abed8992e60f9c470e66b138
ttumzzi <ssumini0829@gmail.com>
2020-02-26 14:59:18 +0900

    연구소 (bfs) *

--- boj/14502.cpp
+++ boj/14502.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
+#include <vector>
 #include <string.h>
 using namespace std;
 
@@ -9,23 +9,23 @@ struct node
     int x, y;
 };
 
-int map[9][9];
-int visit[9][9];
-int n, m, mapSize, answer = -1;
-vector<node> v;
+int n, m, wallNum = 3;
+int map[8][8], visit[8][8];
 int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+int answer = -1;
+vector<node> virus;
 
-node getNode(int i)
+node getNode(int num)
 {
-    return {i / m, i % m};
+    return {num / m, num % m};
 }
 
-int bfs(node w1, node w2, node w3)
+void bfs()
 {
-    queue<node> q;
+    int virNum = 0;
     memset(visit, 0, sizeof(visit));
-    int vir_num = 0;
-    for (auto k : v)
+    queue<node> q;
+    for (auto k : virus)
     {
         q.push(k);
         visit[k.x][k.y] = 1;
@@ -34,54 +34,58 @@ int bfs(node w1, node w2, node w3)
     {
         int x = q.front().x, y = q.front().y;
         q.pop();
-        vir_num++;
+        virNum++;
         for (int i = 0; i < 4; i++)
         {
             int nx = x + dx[i], ny = y + dy[i];
             if (nx < 0 || nx >= n || ny < 0 || ny >= m)
                 continue;
-            if (map[nx][ny] == 1 || visit[nx][ny])
-                continue;
-            if (nx == w1.x && ny == w1.y)
-                continue;
-            if (nx == w2.x && ny == w2.y)
-                continue;
-            if (nx == w3.x && ny == w3.y)
+            if (map[nx][ny] || visit[nx][ny])
                 continue;
             q.push({nx, ny});
-            visit[nx][ny] = 1;
+            visit[nx][ny] = true;
         }
     }
-    return mapSize - vir_num;
+    int safeNum = m * n - virNum - wallNum;
+    answer = safeNum > answer ? safeNum : answer;
+}
+
+void makeWall(int num, vector<int> v)
+{
+
+    node curNode = getNode(num);
+    if (v.size() >= 3)
+    {
+        for (auto k : v)
+            map[getNode(k).x][getNode(k).y] = 1;
+        bfs();
+        for (auto k : v)
+            map[getNode(k).x][getNode(k).y] = 0;
+        return;
+    }
+    if (num == n * m)
+        return;
+    makeWall(num + 1, v);
+    if (!map[curNode.x][curNode.y])
+    {
+        v.push_back(num);
+        makeWall(num + 1, v);
+    }
 }
 
 int main()
 {
     cin >> n >> m;
-    mapSize = n * m - 3;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
         {
             cin >> map[i][j];
             if (map[i][j] == 2)
-                v.push_back({i, j});
+                virus.push_back({i, j});
             if (map[i][j] == 1)
-                mapSize--;
+                wallNum++;
         }
 
-    for (int i = 0; i < n * m - 2; i++)
-    {
-        for (int j = i + 1; j < n * m - 1; j++)
-        {
-            for (int k = j + 1; k < n * m; k++)
-            {
-                node w1 = getNode(i);
-                node w2 = getNode(j);
-                node w3 = getNode(k);
-
-                answer = max(answer, bfs(w1, w2, w3));
-            }
-        }
-    }
+    makeWall(0, {});
     cout << answer << endl;
 }
