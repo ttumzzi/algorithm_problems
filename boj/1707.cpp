ae7e6f0fbd45b0731a45e7a23132d3c7decede78
ttumzzi <ssumini0829@gmail.com>
2020-02-27 18:10:12 +0900

    이분 그래프 @

--- boj/1707.cpp
+++ boj/1707.cpp
@@ -1,56 +1,47 @@
-#include <iostream>
-#include <queue>
-#include <map>
 #include <string.h>
+#include <iostream>
+#include <map>
+#include <queue>
 using namespace std;
 
 map<int, vector<int>> m;
 int visit[20001];
 
-bool bfs(int i)
-{
+bool bfs(int idx) {
     queue<int> q;
-    q.push(i);
-    visit[i] = 1;
-    while (!q.empty())
-    {
+    q.push(idx);
+    visit[idx] = 1;
+    while (!q.empty()) {
         int x = q.front();
         q.pop();
-        for (auto k : m[x])
-        {
-            if (!visit[k])
-            {
+        for (auto k : m[x]) {
+            if (!visit[k]) {
                 q.push(k);
                 visit[k] = -visit[x];
-            }
-            else if (visit[k] == visit[x])
+            } else if (visit[k] == visit[x]) {
                 return false;
+            }
         }
     }
     return true;
 }
 
-int main()
-{
+int main() {
     int t, v, e;
     cin >> t;
-    while (t--)
-    {
-        m.clear();
-        memset(visit, 0, sizeof(visit));
+    while (t--) {
         cin >> v >> e;
-        while (e--)
-        {
-            int a, b;
-            cin >> a >> b;
-            m[a].push_back(b);
-            m[b].push_back(a);
+        memset(visit, 0, sizeof(visit));
+        m.clear();
+        while (e--) {
+            int tmp1, tmp2;
+            cin >> tmp1 >> tmp2;
+            m[tmp1].push_back(tmp2);
+            m[tmp2].push_back(tmp1);
         }
         bool flag = true;
-        for (int i = 1; i <= v; i++)
-        {
-            if (!m[i].empty() && !visit[i] && !bfs(i))
-            {
+        for (int i = 1; i <= v; i++) {
+            if (!visit[i] && !bfs(i)) {
                 flag = false;
                 break;
             }
