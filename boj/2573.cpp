add032d905577bd34a4285af70c8e762159d2147
ttumzzi <ssumini0829@gmail.com>
2020-02-27 18:39:37 +0900

    빙산 @

--- boj/2573.cpp
+++ boj/2573.cpp
@@ -1,109 +1,78 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
 #include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-//코드 정리해서 다시 풀기
-
-int map1[301][301];
-int map2[301][301];
-int visit[301][301];
+struct node {
+    int x, y, h;
+};
 int n, m;
 int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+int map[300][300], visit[300][300];
 
-bool melt(int year)
-{
-    int sea = 0;
-    memset(visit, 0, sizeof(visit));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (year % 2 ? (!map2[i][j]) : (!map1[i][j]))
-            {
-                visit[i][j] = true;
-                if (year % 2)
-                    map1[i][j] = map2[i][j];
-                else
-                    map2[i][j] = map1[i][j];
-                sea++;
-                continue;
-            }
-            int cnt = 0;
-            for (int k = 0; k < 4; k++)
-            {
-                int nx = i + dx[k], ny = j + dy[k];
-                if (year % 2 ? (!map2[nx][ny]) : (!map1[nx][ny]))
-                    cnt++;
-            }
-            if (year % 2)
-            {
-                map1[i][j] = (map2[i][j] - cnt <= 0) ? 0 : (map2[i][j] - cnt);
-                visit[i][j] = !map2[i][j] ? true : false;
-            }
-            else
-            {
-                map2[i][j] = (map1[i][j] - cnt <= 0) ? 0 : (map1[i][j] - cnt);
-                visit[i][j] = !map1[i][j] ? true : false;
+bool melting() {
+    vector<node> v;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (map[i][j]) {
+                int cnt = 0;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k], ny = j + dy[k];
+                    if (!map[nx][ny]) cnt++;
+                }
+                v.push_back({i, j, cnt});
             }
         }
     }
-    return sea == m * n;
+
+    if (v.empty()) return true;
+
+    for (auto k : v) {
+        map[k.x][k.y] -= k.h;
+        map[k.x][k.y] = max(0, map[k.x][k.y]);
+    }
+
+    return false;
 }
 
-void bfs(int x, int y, int year)
-{
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    visit[x][y] = true;
-    while (!q.empty())
-    {
-        int fx = q.front().first, fy = q.front().second;
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = fx + dx[i], ny = fy + dy[i];
-            if (visit[nx][ny])
-                continue;
-            q.push({nx, ny});
-            visit[nx][ny] = true;
-        }
+void dfs(node no) {
+    if (visit[no.x][no.y]) return;
+    visit[no.x][no.y] = true;
+    for (int i = 0; i < 4; i++) {
+        int nx = no.x + dx[i], ny = no.y + dy[i];
+        if (map[nx][ny]) dfs({nx, ny});
     }
 }
 
-int main()
-{
+int main() {
     int year = 0;
+
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            cin >> map1[i][j];
+            cin >> map[i][j];
 
-    while (true)
-    {
-        if (melt(year))
-        {
+    while (true) {
+        year++;
+        if (melting()) {
             cout << 0 << endl;
             return 0;
         }
+        memset(visit, 0, sizeof(visit));
+
         int cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (!visit[i][j])
-                {
-                    if (cnt >= 1)
-                    {
-                        cout << year;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] && !visit[i][j]) {
+                    cnt++;
+                    if (cnt >= 2) {
+                        cout << year << endl;
                         return 0;
                     }
-                    bfs(i, j, year);
-                    cnt++;
+                    dfs({i, j});
                 }
             }
         }
-        year++;
     }
 }
