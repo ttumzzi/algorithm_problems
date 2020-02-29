5f1c00cc34199c1799195c0f5b6f43ba03ed69bd
ttumzzi <ssumini0829@gmail.com>
2020-02-29 19:03:36 +0900

    피보나치 함수 (dp)

---
+++ boj/1003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int d[41][2];
+
+int main() {
+    d[0][0] = 1, d[0][1] = 0;
+    d[1][0] = 0, d[1][1] = 1;
+    for (int i = 2; i <= 40; i++) {
+        d[i][0] = d[i - 1][0] + d[i - 2][0];
+        d[i][1] = d[i - 1][1] + d[i - 2][1];
+    }
+
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << d[n][0] << ' ' << d[n][1] << endl;
+    }
+}
