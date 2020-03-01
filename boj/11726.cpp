7ce1e071e609f929da11ea5749e8e0a74f442c32
ttumzzi <ssumini0829@gmail.com>
2020-03-01 15:27:48 +0900

    2xn 타일링 (dp) @

--- boj/11726.cpp
+++ boj/11726.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
-#define mod 10007
-
-int d[1001];
-
-// 더 효율적인 코드로 만들어보기
 
 int main() {
-    d[1] = 1, d[2] = 2;
-    for (int i = 3; i <= 1000; i++)
-        d[i] = (d[i - 1] + d[i - 2]) % mod;
     int n;
     cin >> n;
-    cout << d[n] % mod << endl;
+    int a = 1, b = 2, c = 3;
+    for (int i = 4; i <= n; i++) {
+        a = b, b = c;
+        c = (a + b) % 10007;
+    }
+    cout << (n <= 3 ? n : c);
 }
