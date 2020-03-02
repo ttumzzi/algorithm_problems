6775f75482195e4bb613ff887d850992bbc00496
ttumzzi <ssumini0829@gmail.com>
2020-03-02 14:42:13 +0900

    정수 삼각형 (dp) @

--- boj/1932.cpp
+++ boj/1932.cpp
@@ -1,31 +1,34 @@
 #include <algorithm>
 #include <iostream>
-#include <queue>
 using namespace std;
 
-struct node {
-    int x, y, sum;
-};
-int a[501][501], d[501][501];
+int a[500][500], d[500][500];
+int n;
+
+int solve() {
+    int answer = 0;
+    d[0][0] = a[0][0];
+    for (int i = 1; i < n; i++)
+        for (int j = 0; j <= i; j++) {
+            if (j == 0)
+                d[i][j] += d[i - 1][j];
+            else if (j == i)
+                d[i][j] += d[i - 1][j - 1];
+            else
+                d[i][j] += max(d[i - 1][j - 1], d[i - 1][j]);
+        }
+
+    for (int i = 0; i < n; i++) answer = max(answer, d[n - 1][i]);
+    return answer;
+}
 
 int main() {
-    int n, answer = 0;
     cin >> n;
     for (int i = 0; i < n; i++)
         for (int j = 0; j <= i; j++) {
             cin >> a[i][j];
-            if (i == 0)
-                d[i][j] = a[i][j];
-            else if (j == 0)
-                d[i][j] = d[i - 1][j] + a[i][j];
-            else if (j == i)
-                d[i][j] = d[i - 1][j - 1] + a[i][j];
-            else
-                d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + a[i][j];
+            d[i][j] = a[i][j];
         }
 
-    for (int i = 0; i < n; i++)
-        answer = max(answer, d[n - 1][i]);
-
-    cout << answer;
+    cout << solve();
 }
