f405fd22323e15a32af0ac000ac66b62e9550c3b
ttumzzi <ssumini0829@gmail.com>
2020-03-03 16:45:10 +0900

    쉬운 계단 수 (dp) @

--- boj/10844.cpp
+++ boj/10844.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 using namespace std;
 #define mod 1000000000
-long long a[101][10] = {
-    0,
-};
+
+int d[101][10];
 
 int main() {
-    int n;
+    int n, answer = 0;
     cin >> n;
 
-    for (int i = 1; i <= 9; i++) a[1][i] = 1;
+    for (int i = 1; i < 10; i++) d[1][i] = 1;
+    for (int i = 2; i <= n; i++)
+        for (int j = 0; j < 10; j++) {
+            if (j == 0)
+                d[i][j] = d[i - 1][1];
+            else if (j == 9)
+                d[i][j] = d[i - 1][8];
+            else
+                d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % mod;
+        }
 
-    for (int i = 2; i <= n; i++) {
-        a[i][0] = a[i - 1][1];
-        for (int j = 1; j <= 8; j++)
-            a[i][j] = (a[i - 1][j - 1] + a[i - 1][j + 1]) % mod;
-        a[i][9] = a[i - 1][8];
-    }
-
-    long long answer = 0;
-    for (int i = 0; i < 10; i++)
-        answer = (answer + a[n][i]) % mod;
-    cout << answer % mod;
+    for (int i = 0; i < 10; i++) answer = (answer + d[n][i]) % mod;
+    cout << answer;
 }
