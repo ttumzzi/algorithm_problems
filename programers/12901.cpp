d9d2b9688f350cf516136e89b76f180bd423d1f1
ttumzzi <ssumini0829@gmail.com>
2020-03-11 23:30:30 +0900

    2016년

---
+++ programers/12901.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string day[7] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+
+string solution(int a, int b)
+{
+    string answer = "";
+    int days = b;
+    for (int i = 1; i < a; i++)
+        days += month[i];
+    answer = day[days % 7];
+    return answer;
+}
+
+int main()
+{
+    cout << solution(5, 24) << endl;
+    cout << solution(1, 1) << endl;
+}
